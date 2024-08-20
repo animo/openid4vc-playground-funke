bf9f148f3ef256a0a7c63dae436e952559808788
Martin Auer <martin.auer97@gmail.com>
2024-08-20 10:45:31 +0200

    feat: add mdoc pid presentation option

--- app/components/VerifyTab.tsx
+++ app/components/VerifyTab.tsx
@@ -7,9 +7,19 @@ export function VerifyTab() {
       presentationDefinition: {
         id: crypto.randomUUID(),
         name: 'PID Credential request for C',
+        submission_requirements: [
+          {
+            name: 'Person Identification Data (PID) Request',
+            purpose: 'We need your Person Identification Data to prove that we can process the submitted data.',
+            rule: 'pick',
+            count: 1,
+            from: 'A',
+          },
+        ],
         input_descriptors: [
           {
             id: crypto.randomUUID(),
+            group: ['A'],
             constraints: {
               limit_disclosure: 'preferred',
               fields: [
@@ -50,6 +60,36 @@ export function VerifyTab() {
             },
             name: 'PID Name',
             purpose: 'Verify your name',
+          },
+          {
+            id: 'eu.europa.ec.eudi.pid.1', // TODO: DOUBLE CHECK
+            group: ['A'],
+            format: {
+              mso_mdoc: {
+                alg: ['ES256', 'ES384', 'ES512', 'EdDSA'], // alg: ['ES256', 'ES384', 'ES512', 'EdDSA', 'ESB256', 'ESB320', 'ESB384', 'ESB512'],
+              },
+            },
+            constraints: {
+              fields: [
+                {
+                  path: ["$['eu.europa.ec.eudi.pid.1']['given_name']"],
+                  intent_to_retain: false,
+                },
+                {
+                  path: ["$['eu.europa.ec.eudi.pid.1']['family_name']"],
+                  intent_to_retain: false,
+                },
+                {
+                  path: ["$['eu.europa.ec.eudi.pid.1']['age_equal_or_over.21']"],
+                  intent_to_retain: false,
+                },
+                {
+                  path: ["$['eu.europa.ec.eudi.pid.1']['nationalities']"],
+                  intent_to_retain: false,
+                },
+              ],
+              limit_disclosure: 'required',
+            },
           },
         ],
       },
