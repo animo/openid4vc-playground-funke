87ca64829726dd164fbab8397c78e9960dbfda16
Berend Sliedrecht <sliedrecht@berend.io>
2024-08-26 16:48:36 +0200

    fix: include patcjes
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- app/components/VerifyTab.tsx
+++ app/components/VerifyTab.tsx
@@ -1,7 +1,15 @@
-import { createRequest, getIssuer } from '../lib/api'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@radix-ui/react-tooltip'
+import { useEffect, useState } from 'react'
+import { createRequest, getIssuer, getX509Certificate } from '../lib/api'
 import { VerifyBlock } from './VerifyBlock'
 
 export function VerifyTab() {
+  const [x509Certificate, setX509Certificate] = useState<string>()
+
+  useEffect(() => {
+    getX509Certificate().then(({ certificate }) => setX509Certificate(certificate))
+  }, [])
+
   const createRequestForVerification = async () => {
     const issuer = (await getIssuer()).availableX509Certificates[0]
     return await createRequest({
@@ -65,6 +73,28 @@ export function VerifyTab() {
         flowName="Signed Credential (C or C') or Authenticated Channel (B') with SD-JWT"
         createRequest={createRequestForVerification}
       />
+      <div style={{ height: '30px' }} />
+      <div className="flex justify-center flex-col items-center bg-gray-200 min-h-64 w-full rounded-md p-7">
+        <h3>X.509 Certificate in base64 format</h3>
+        <TooltipProvider>
+          <Tooltip>
+            <div className="flex flex-col p-5 gap-2 justify-center items-center gap-6">
+              <TooltipTrigger asChild>
+                <p
+                  onClick={(e) => navigator.clipboard.writeText(e.currentTarget.innerText)}
+                  className="text-gray-500 break-all cursor-pointer"
+                >
+                  {x509Certificate ?? 'No X.509 Certificate found'}
+                </p>
+              </TooltipTrigger>
+            </div>
+
+            <TooltipContent>
+              <p>Click to copy</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </>
   )
 }
